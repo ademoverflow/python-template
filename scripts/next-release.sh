c64f0cf93de903bcda5b654b95e6469389fb0807
Adem Usta <adem@antipodestudios.io>
2023-08-17 15:24:39 +0200

    fix: use poetry venv in script

--- scripts/next-release.sh
+++ scripts/next-release.sh
@@ -3,7 +3,7 @@
 git config --global user.name "github-actions"
 git config --global user.email "action@github.com"
 
-VERSION=$(semantic-release print-version)
+VERSION=$(poetry run semantic-release print-version)
 echo "Publishing next release ${VERSION} ..."
 
-${PACKAGE_NAME} semantic-release publish
+${PACKAGE_NAME} poetry run semantic-release publish
