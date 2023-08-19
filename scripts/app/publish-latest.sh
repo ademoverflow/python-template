358de1b22db115db6b43f62927785d3ad680d439
Adem Usta <adem@antipodestudios.io>
2023-08-19 19:26:41 +0200

    feat: prepare files for application template

---
+++ scripts/app/publish-latest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME=python-template
+
+# TODO: publish docker image python-template:production to our AWS registry with the "latest" tag.
+echo "Nothing to do here (for now).
